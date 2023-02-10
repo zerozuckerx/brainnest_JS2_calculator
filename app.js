d0e6deea1d7391e4dde0b8748a797aaefbc9fb39
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2023-02-10 14:24:45 +0100

    added clear(), numbe rpopulation

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let display = document.querySelector(".display");
+display.textContent = "0";
+
 function add(num1, num2) {
   return num1 + num2
 }
@@ -16,4 +19,27 @@ function divide(num1, num2) {
 
 function operate(operator, num1, num2) {
   return operator(num1, num2);
+}
+
+function clear() {
+  display.textContent = "0";
+}
+
+document.querySelector(".clear").addEventListener("click", () => clear());
+
+function toDisplay(input) {
+  let variable =
+  displayValue
+}
+
+let numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    console.log(button.textContent);
+    setDisplay(button.textContent);
+  })});
+
+function setDisplay(input) {
+  let displayValue = document.querySelector(".display")
+  displayValue.textContent = input;
 }
