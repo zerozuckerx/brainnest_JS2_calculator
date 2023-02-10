02c7e3ddca68bd88cae1d324a8468858cb4b74cd
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2023-02-10 18:27:15 +0100

    display population for single number

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-let display = document.querySelector(".display");
-display.textContent = "0";
+//display
+let displayValue = document.querySelector(".display");
+displayValue.textContent = "0";
+//num1,num2
+let currentNumber = [];
 
 function add(num1, num2) {
-  return num1 + num2
+  return num1 + num2;
 }
 
 function subtract(num1, num2) {
-  return num1 - num2
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-  return num1 * num2
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
-  return num1 / num2
+  return num1 / num2;
 }
 
 function operate(operator, num1, num2) {
@@ -22,24 +25,20 @@ function operate(operator, num1, num2) {
 }
 
 function clear() {
-  display.textContent = "0";
+  displayValue.textContent = "0";
 }
 
 document.querySelector(".clear").addEventListener("click", () => clear());
 
-function toDisplay(input) {
-  let variable =
-  displayValue
-}
-
 let numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach(button => {
   button.addEventListener("click", () => {
-    console.log(button.textContent);
-    setDisplay(button.textContent);
+    currentNumber.push(button.textContent);
+    // num.join("");
+    // console.log(num.join(""));
+    toDisplay(currentNumber.join(""));
   })});
 
-function setDisplay(input) {
-  let displayValue = document.querySelector(".display")
+function toDisplay(input) {
   displayValue.textContent = input;
 }
